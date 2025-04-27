0f313c776d0570bf1408489e0bd752e100d81ba5
Satyatej10 <satyatejinturi@gmail.com>
2025-04-27 13:15:10 +0530

    ui for users -components

---
+++ client/src/users/user-components/CartItem.jsx
@@ -0,0 +1,13 @@
+function CartItem({ item }) {
+    return (
+      <div className="border-b py-2">
+        <h4 className="text-lg">{item.service.name}</h4>
+        <p>Category: {item.service.category}</p>
+        <p>Price: ₹{item.service.price}</p>
+        <p>Quantity: {item.quantity}</p>
+        <p>Estimated Time: {item.service.timeEstimated} hours</p>
+      </div>
+    );
+  }
+  
+  export default CartItem;
