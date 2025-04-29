75cc55d07e790e67fe3d9a33b69cfa5cb00c7f7c
DSP <dsp771122@gmail.com>
2025-04-29 19:26:08 +0530

    service routes

---
+++ server/routes/serviceRoute.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const router=express.Router();
+const {getService,getServicesByCategory} =require('../controllers/serviceController');
+
+router.get('/',getService);
+router.get('/category/:categoryName',getServicesByCategory);
--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const dotenv=require('dotenv');
+const serviceRoutes=require('./routes/serviceRoute');
 
 const app=express();
+app.use(cors());
 dotenv.config();
+
+app.use('/service',serviceRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
 })
+
