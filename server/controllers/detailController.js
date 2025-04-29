3369790425ef90c4be59d74bc322bfe2fd193a95
Satyatej10 <satyatejinturi@gmail.com>
2025-04-30 00:00:22 +0530

    `Updated controllers and routes for detail and search functionality`

--- server/controllers/detailController.js
+++ server/controllers/detailController.js
@@ -3,12 +3,31 @@ const Service = require('../models/Service');
 
 const detail=async (req,res)=>{
     try{
-        const name=req.query.name;
+        const {name}=req.query;
+        const temp=await Service.find({category:name});
+        const arr=temp.map((i)=>i.name);
+        const data=Vendor.find(
+            {
+                category:name,
+                services: { $in: arr }
+            }
+        );
+        const ans=data.map(i=>{
+            const help=temp.filter(j=>
+                data.services.includes(j.name)
+            )
+            return {
+                ...data.toObject(),
+                services: help,
+            };
+        });
 
-        const data=await Vendor.find({category:name}).populate('services');
+        res.status(200).json(ans);
     }
 
     catch (error) {
         res.status(500).json({ error: error.message });
       }
 }
+
+module.exports={detail};
--- server/routes/detailRoute.js
+++ server/routes/detailRoute.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-const detail=require('../controllers/detailController');
+const {detail}=require('../controllers/detailController');
 
 router.get('/',detail);
 
