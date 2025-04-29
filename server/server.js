2a1614ac2178d6ce77b66298d61852537ba31bfe
DSP <dsp771122@gmail.com>
2025-04-29 19:40:16 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose=require('mongoose');
 const cors=require('cors');
 const dotenv=require('dotenv');
 const serviceRoutes=require('./routes/serviceRoute');
+const vendorRoutes=require('./routes/vendorRoute');
 const db=require('./config/connectDB');
 const app=express();
 app.use(cors());
@@ -10,6 +11,7 @@ dotenv.config();
 db();
 
 app.use('/service',serviceRoutes);
+app.use('/vendor',vendorRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
