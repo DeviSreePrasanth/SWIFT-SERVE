9ba5e1ad4d8894d22cbb9ffd1cebfc0733f36c77
Satyatej10 <satyatejinturi@gmail.com>
2025-04-29 19:43:44 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,13 +3,16 @@ const mongoose=require('mongoose');
 const cors=require('cors');
 const dotenv=require('dotenv');
 const serviceRoutes=require('./routes/serviceRoute');
+const searchRoutes=require('./routes/searchRoute');
 const db=require('./config/connectDB');
 const app=express();
 app.use(cors());
+app.use(express.json());
 dotenv.config();
 db();
 
 app.use('/service',serviceRoutes);
+app.use('/search',searchRoutes);
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
