f0211c70863900fe9486a5f02e3324b9964c50f2
DSP <dsp771122@gmail.com>
2025-04-29 21:56:36 +0530

    updated service.js

--- server/models/Service.js
+++ server/models/Service.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const serviceSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  category: { type: String, required: true }
+  category: { type: String, required: true },
+  imageUrl: { type: String } 
 });
 
 module.exports = mongoose.model('Service', serviceSchema);
