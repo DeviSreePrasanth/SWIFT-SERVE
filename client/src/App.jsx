499f1ea3ec603557ba8019a40a8b2f499f94d332
DSP <dsp771122@gmail.com>
2025-04-30 16:36:32 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import ServicesPage from "./pages/ServicePage";
-import ServiceDetails from "./pages/ServiceDetails";
-import BookingConfirmation from "./pages/BookingConfirmation";
-import Profile from "./pages/Profile";
-import VendorPage from "./pages/VendorPage";
-import SearchResults from "./pages/SearchResultsPage";
-import ErrorBoundary from './components/ErrorBoundary';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ServicesPage from './pages/ServicePage';
+import ServiceDetails from './pages/ServiceDetails';
+import BookingConfirmation from './pages/BookingConfirmation';
+import Profile from './pages/Profile';
+import VendorPage from './pages/VendorPage';
+import Vendor from './pages/Vendor';
+import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -15,16 +16,14 @@ function App() {
       <div className="min-h-screen flex flex-col">
         <main className="flex-1">
           <Routes>
-            <Route path="/" element={<Home />} />{" "}
-            <Route path="/searchQuery/:name" element={<ErrorBoundary><SearchResults /></ErrorBoundary>}/>
+            <Route path="/" element={<Home />} />
             <Route path="/services" element={<ServicesPage />} />
             <Route path="/service/:id" element={<ServiceDetails />} />
             <Route path="/vendor" element={<VendorPage />} />
-            <Route
-              path="/booking-confirmation"
-              element={<BookingConfirmation />}
-            />
+            <Route path="/booking-confirmation" element={<BookingConfirmation />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/category/:category" element={<Vendor/>}/>
+            <Route path="/cart/:userId" element={<CartPage/>}/>
           </Routes>
         </main>
 
