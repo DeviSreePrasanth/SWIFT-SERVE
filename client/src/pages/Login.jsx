cf5ecf94273ef94b26a03747a413771cc305b7f2
DSP <dsp771122@gmail.com>
2025-05-01 13:58:24 +0530

    token validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -44,6 +44,7 @@ const Login = () => {
 
       if (response.data.success) {
         const { token, user } = response.data;
+        console.log(`${token}`);
         localStorage.setItem("authToken", token);
         localStorage.setItem("userId", user.id);
         localStorage.setItem("userName", user.name);
